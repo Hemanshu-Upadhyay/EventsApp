cbcb6ffa508b74e74333ab99a567d0f33c24a8a4
Yngesh-Raman-QED42 <yngesh.raman@qed42.com>
2023-06-01 14:11:03 +0530

    add date and time of event creation

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,3 +2,18 @@ export const formatTime = (epochTime: number) => {
   const formattedTime = new Date(epochTime).toISOString();
   return formattedTime;
 };
+
+export const formatISOToDateString = (dateString: string) => {
+  const date = new Date(dateString);
+  const options = {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  };
+  const formattedDateTime = date.toLocaleString(undefined, options);
+
+  return formattedDateTime;
+};
